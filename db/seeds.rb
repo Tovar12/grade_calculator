d3277a03d5a8572f1931eba0ef0e8db1765bf545
Felipe Tovar Ospina <FelipeTovarMac@Felipes-MacBook-Air.local>
2016-08-21 15:55:21 -0500

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Subject.create(name: 'Fármaco')
+Grade.create(name: 'Parcial #1', percentage: 0.2, subject_id: 1)
+Grade.create(name: 'Parcial #2', percentage: 0.2, subject_id: 1)
+Grade.create(name: 'Parcial #3', percentage: 0.3, subject_id: 1)
 Grade.create(name: 'Seguimiento #1', percentage: 0.1, subject_id: 1)
 Grade.create(name: 'Seguimiento #2', percentage: 0.1, subject_id: 1)
 Grade.create(name: 'Seguimiento #3', percentage: 0.1, subject_id: 1)
